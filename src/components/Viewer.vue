9eefe87805e717f353cb09d78b19686f0e07a40b
imucho <26864686+imucho@users.noreply.github.com>
2019-03-11 19:56:29 +0900

    viewerにちょいと追加

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -6,4 +6,30 @@
 </template>
 
 <script>
+import { createNamespacedHelpers } from 'vuex'
+const profileHelper = createNamespacedHelpers('profile')
+const skillHelper = createNamespacedHelpers('skill')
+export default {
+    created(){
+        console.log(this.name)
+        console.log(this.company)
+        console.log(this.job)
+
+        console.log(this.common)
+        console.log(this.unique)
+        console.log(this.numerical)
+    },
+    computed: {
+        ...profileHelper.mapState({
+            name: state => state.name,
+            company: state => state.company,
+            job: state => state.job
+        }),
+        ...skillHelper.mapState({
+            common: state => state.common,
+            unique: state => state.unique,
+            numerical: state => state.numerical
+        })
+    }
+}
 </script>
